94be592d1d79b73fdb20ee0d8744d3c92edc5ae3
Gokul Krishnan <gokul@localhost.com>
2011-03-23 08:29:28 +1100

    Added javadocs to interfaces

--- src/main/java/org/sqlrecorder/events/filter/SqlOutputFilter.java
+++ src/main/java/org/sqlrecorder/events/filter/SqlOutputFilter.java
@@ -1,9 +1,17 @@
 package org.sqlrecorder.events.filter;
 
 public interface SqlOutputFilter {
+
+    /**
+     *
+     * @return An string that identifies this filter
+     */
     String id();
 
-    //if it returns true, the sql will not be logged to o/p(console,file etc..)
+    /**
+     * @param sql the sql to filter
+     * @return true , if the the sql will not be logged to o/p(console,file etc..) false otherwise
+     */
     boolean filter(String sql);
 }
 
--- src/main/java/org/sqlrecorder/events/listener/StatementListener.java
+++ src/main/java/org/sqlrecorder/events/listener/StatementListener.java
@@ -5,9 +5,35 @@ import java.util.List;
 import org.sqlrecorder.events.event.ExecuteEvent;
 
 public interface StatementListener {
+
+    /**
+     * @return string identifying this listener
+     */
     String id();
+
+    /**
+     * @param e The query that was just executed is encapsulated as an ExecuteEvent. Retrieve
+     *          the details and apply filters if necessary to log it.
+     *
+     *          Listeners can be configured with as many filters as it needs at startup time. Filters
+     *          will be applied in this method.
+     */
     void queryExecuted(ExecuteEvent e);
-    List<String> executedQueries();
-    boolean returnsExecutedQueries();
+
+    /**
+     * Perform any cleanup class when the app is shutting down listeners(like closing files etc..)
+     */
     void shutDown();
+
+    /**
+     * Experimental features
+     * @return empty list for now
+     */
+    List<String> executedQueries();
+
+    /**
+     * Experimental features
+     * @return false for now
+     */
+    boolean returnsExecutedQueries();
 }
